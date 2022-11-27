47bbd2d34a89969810265c51961b6e57901ca974
Nuroferatu <32645518+Nuroferatu@users.noreply.github.com>
2022-11-28 00:15:31 +0100

    Concept #1

--- include/configlib.h
+++ include/configlib.h
@@ -15,7 +15,42 @@
 #ifndef __STF_CONFIGLIB_H__
 #define __STF_CONFIGLIB_H__
 
+#include <string>
+
 namespace stf {
+
+// Concept - Want something that will work a bit like console variables in games
+enum class eSettingLevel {
+    APP,        // Application level setting is defined in application and can not be modified by config file or by user when app is running - it's read only setting
+    SYS,        // System level can be modyfied only by config file that cames from system directory - it's read only
+    USER        // User level can be modified by user and system configuration files - it's RW and might also be saved back to config file
+};
+
+
+template<typename T>
+class SettingParam {
+public:
+    using type = T;
+
+    //    enum class Type { BOOL, INT, FLOAT, STRING };
+    SettingParam( const std::string& name, eSettingLevel level, T defaultValue ) : _name(name), _level(level), _defaultVal(defaultValue), _val(defaultValue) {}
+
+    const std::string&  getName( void ) const { return _name; }
+    T   getVal( void ) const { return _val; }
+
+private:
+    // Once set can not be changed
+    const std::string   _name;
+    eSettingLevel   _level;
+    T       _defaultVal;
+    T       _val;
+};
+
+// Container for SettingParam
+class Settings {
+public:
+
+};
 
 }; // ns:stf
 
--- sandbox/main.cpp
+++ sandbox/main.cpp
@@ -12,13 +12,27 @@
 // |                   http://softwaretoolsfactory.com                       |
 // '-------------------------------------------------------------------------'
 #include <iostream>
+#include "configlib.h"
+
+static stf::SettingParam<bool>          sampleBoolParam{ "sampleBoolParam", stf::eSettingLevel::APP, false };
+static stf::SettingParam<std::uint32_t> sampleIntParam{ "sampleIntParam", stf::eSettingLevel::SYS, 100 };
+static stf::SettingParam<float>         sampleFloatParam{ "sampleFloatParam", stf::eSettingLevel::USER, 1.1234f };
+static stf::SettingParam<std::string>   sampleStringParam{ "sampleFloatParam", stf::eSettingLevel::USER, "sampleDefaultValue" };
 
 int main( int argc, char* argv, char* env[] ) {
+    // Planed usage
+    // 1. Direct in place of declatarion
+    bool bVal = sampleBoolParam.getVal();
+    std::cout << bVal << std::endl;
+
+//    int iVal = stf::Settings::Get().Find( "sampleIntParam" ).GetVal();
+//    stf::Settings::Get().Find( "sampleStringParam" ).SetVal( "otherValue" );
 
     return 0;
 }
 
 #ifdef _WIN32
+
 #include <windows.h>
 
 int CALLBACK WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow ) {
